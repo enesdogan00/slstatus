f919635f0ed16ac3c86bf053dbfc68c3d266154e
enes4949 <enesdogan26@gmail.com>
2021-05-15 21:08:57 +0300

    music go brrr

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
     /* function, format, argument */
-    {run_command, "[%s]", "mocp -Q '%artist - %song'"},
+    {run_command, "[%s]", "cat /dev/shm/song"},
     {cpu_perc, "[CPU:%s%|", NULL},
     {temp, "%2s°]",
      "/sys/devices/platform/coretemp.0/hwmon/hwmon0/temp2_input"},
